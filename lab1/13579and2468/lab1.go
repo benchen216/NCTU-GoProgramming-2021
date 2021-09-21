4b582ee071c2269b9549900335d8d59e2092744f
a13579and2468 <a13579and2468@a13579and2468-X555LD>
2021-09-21 23:23:16 +0800

    lab1

--- lab1/13579and2468/lab1.go
+++ lab1/13579and2468/lab1.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+func Add(a, b int) int {
+	return a + b
+}
+func Sub(a, b int) int {
+	return a - b
+}
+func Mul(a, b int) int {
+	return a * b
+}
+func Div(a, b int) int {
+	return a / b
+}
 func main() {
 	// Please complete the code to make this program compiled without error.
 	// Notice that you can only add code in this file.
@@ -12,12 +24,12 @@ func main() {
 	fmt.Println("2) Sub")
 	fmt.Println("3) Mul")
 	fmt.Println("4) Div")
-
+	var action int
+	var a, b int
 	fmt.Println("Please input your action: ")
 	fmt.Scanln(&action)
 	fmt.Println("Please input two numbers: ")
 	fmt.Scanln(&a, &b)
-
 	switch action {
 	case 1:
 		fmt.Println(Add(a, b))
@@ -30,5 +42,4 @@ func main() {
 	default:
 		fmt.Println("Wrong input!!")
 	}
-
 }
