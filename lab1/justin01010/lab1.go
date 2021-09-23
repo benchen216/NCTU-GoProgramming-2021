0d45ef775b8ab4482c2603e4782d2babb1023808
justin01010 <justinimbus@yahoo.com.tw>
2021-09-23 16:31:37 +0800

    lab1

---
+++ lab1/justin01010/lab1.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func Sub(x, y int) int {
+	return x - y
+}
+
+func Mul(x, y int) int {
+	return x * y
+}
+
+func Div(x, y int) int {
+	return x / y
+}
+
+func main() {
+	// Please complete the code to make this program compiled without error.
+	// Notice that you can only add code in this file.
+
+	fmt.Println("1) Add")
+	fmt.Println("2) Sub")
+	fmt.Println("3) Mul")
+	fmt.Println("4) Div")
+
+	var action, a, b int
+
+	fmt.Println("Please input your action: ")
+	fmt.Scanln(&action)
+	fmt.Println("Please input two numbers: ")
+	fmt.Scanln(&a, &b)
+
+	switch action {
+	case 1:
+		fmt.Println(Add(a, b))
+	case 2:
+		fmt.Println(Sub(a, b))
+	case 3:
+		fmt.Println(Mul(a, b))
+	case 4:
+		fmt.Println(Div(a, b))
+	default:
+		fmt.Println("Wrong input!!")
+	}
+
+}
