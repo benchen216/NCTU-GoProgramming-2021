201fe9626257d91412d3e8b3ea21329187e09cc6
ben <benchen216@gmail.com>
2021-09-30 10:30:29 +0800

    more hint

--- lab2/YourGithubUsername/lab2.go
+++ lab2/YourGithubUsername/lab2.go
@@ -1,6 +1,17 @@
 package main
 
+import "fmt"
+
+func Sum() {
+
+}
+
 func main() {
 	// Please complete the code to make this program be compiled without error.
-	// Notice that you can only add code in this file.go
+	// Notice that you can only add code in this file.
+	var n int64
+	for {
+		fmt.Scanln(&n)
+
+	}
 }
