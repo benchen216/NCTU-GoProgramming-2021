9d9b1565537843a8f2b131ca423825679eb82670
alex10101025 <alex10101025@gmail.com>
2021-09-30 16:06:05 +0800

    lab2

--- lab2/lab2.go
+++ lab2/lab2.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Sum() {
-
+func Sum(n int64) int64 {
+	var total, i int64
+	for i = 1; i <= n; i++ {
+		if i%7 == 0 {
+			continue
+		}
+		if i > 1 {
+			fmt.Printf("+%d", i)
+		} else {
+			fmt.Printf("%d", i)
+		}
+		total += i
+	}
+	return total
 }
 
 func main() {
@@ -12,6 +26,11 @@ func main() {
 	var n int64
 	for {
 		fmt.Scanln(&n)
-
+		if n > 0 {
+			total := Sum(n)
+			fmt.Printf("=%d\n", total)
+		} else {
+			break
+		}
 	}
 }
