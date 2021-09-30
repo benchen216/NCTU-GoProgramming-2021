79d64b9a36f2db9ff97d41715370b1fc8948886d
andytsai <andytsai2000@gmail.com>
2021-09-30 16:20:10 +0800

    submit

--- lab2/lab2.go
+++ lab2/lab2.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func Sum() {
 
@@ -9,9 +12,22 @@ func Sum() {
 func main() {
 	// Please complete the code to make this program be compiled without error.
 	// Notice that you can only add code in this file.
-	var n int64
+	var n int
 	for {
 		fmt.Scanln(&n)
+		if n == 0 {
+			break
+		}
+		var ans string = "1"
+		var sum int = 1
+		for i := 2; i <= n; i++ {
+			if i%7 != 0 {
+				ans += "+" + strconv.Itoa(i)
+				sum += i
+			}
+		}
+		ans += "=" + strconv.Itoa(sum)
 
+		fmt.Println(ans)
 	}
 }
