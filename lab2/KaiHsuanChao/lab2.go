def3d3127f7cafbd03662ced59e5a7f134868753
KaiHsuanChao <kaisyuan.sc09@nycu.edu.tw>
2021-10-01 20:47:28 +0800

    Lab2

---
+++ lab2/KaiHsuanChao/lab2.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func Sum(n int64) {
+	sum := int64(0)
+	if n%7!=0{
+		for a := int64(1); a <= n; a++ {
+			if a%7 == 0 {
+				continue
+			}
+			if a != n {
+				fmt.Print(a, "+")
+			}
+			sum = sum + a
+		}
+		fmt.Println(n,"=",sum)
+	}
+	if n%7 == 0{
+		for a := int64(1); a <= n; a++ {
+			if a%7 == 0 {
+				continue
+			}
+			if a != n && (a+1)!=n{
+				fmt.Print(a,"+")
+			}else{
+				fmt.Print(a)
+			}
+			sum = sum + a
+		}
+		fmt.Println("=",sum)
+	}
+}
+
+func main() {
+	// Please complete the code to make this program be compiled without error.
+	// Notice that you can only add code in this file.
+	var n int64
+	for {
+		fmt.Println("Enter n.")
+		fmt.Scanln(&n)
+
+		if n==0{
+			break
+		}
+		if n!=0 {
+			Sum(n)
+		}
+
+	}
+}
