fa04dd68a617f7ddcc40e94e75ce5daf3eb276ac
GuanJie <sa871018@gmail.com>
2021-10-03 13:30:32 +0800

    0612203-attempt1

---
+++ lab2/liao1018/lab2.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func Sum(n int64) {
+	var sum = int64(0)
+	
+	for a := int64(1); a <= n; a++{
+		if a%7 == 0 {
+			continue
+		} else {
+			if(a == 1){
+				fmt.Print(a)
+			} else {
+				fmt.Print("+",a)
+			}
+			sum = sum + a
+		}
+	} 
+	fmt.Println("=",sum)
+}
+
+func main() {
+	// Please complete the code to make this program be compiled without error.
+	// Notice that you can only add code in this file.
+	var n int64
+	for {
+		fmt.Println("Enter a int.")
+		fmt.Scanln(&n)
+
+		if n==0{
+			break
+		}
+		if n!=0 {
+			Sum(n)
+		}
+
+	}
+}
