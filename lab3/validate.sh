3be949a5aa157b0d0302ed7c485b66f7315c5f8b
a13579and2468 <a13579and2468@a13579and2468-X555LD>
2021-10-06 20:01:58 +0800

    lab3

--- lab3/validate.sh
+++ lab3/validate.sh
@@ -2,20 +2,23 @@
 
 test_path="${BASH_SOURCE[0]}"
 solution_path="$(realpath .)"
-tmp_dir=$(mktemp -d -t lab3-XXXXXXXXXX)
+tmp_dir=$(mktemp -d -t lab4-XXXXXXXXXX)
 
 echo "working directory: $tmp_dir"
 cd $tmp_dir
 
 #rm -rf *
-cp $solution_path/lab3.go .
+cp $solution_path/lab4.go .
 result=$(go run lab3.go 2>&1) ; ret=$?
 if [ $ret -ne 0 ] ; then
   echo "\"go run lab3.go\" fails ; NO POINT"
 else
-  echo "\"go run lab4.go\" output: \"$result\""
-  if [ $(python3 -c 'print("沒夾到喔！\n"*50)') != "$(echo $result)" ] ; then
+  echo "\"go run lab3.go\" output: \"$result\""
+  ans=$(python3 -c 'print("沒夾到喔！\n"*50)')
+  if [ "$ans" != "$result" ] ; then
     echo "wrong answer ; NO POINT"
+    echo "答案錯了"
+    exit(1)
   else
     echo "GET POINT 1"
   fi
