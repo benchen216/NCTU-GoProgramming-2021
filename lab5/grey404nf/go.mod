3932328c69444de70eedb987c8dd8d05eaf7f8cb
grey68103 <yuan68103@gmail.com>
2021-10-14 18:12:01 +0800

    grey404nf-lab5-attempt1

---
+++ lab5/grey404nf/go.mod
@@ -0,0 +1,3 @@
+module go-web
+
+go 1.17
---
+++ lab5/grey404nf/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func gcd(a, b int) int {
+	for b!=0 {
+		tmp:=b
+		b=a%b
+		a=tmp
+	}
+	return a
+}
+
+func lab5(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	op := r.Form["op"][0]
+	num1, _ := strconv.Atoi(r.Form["num1"][0])
+	num2, _ := strconv.Atoi(r.Form["num2"][0])
+	
+	switch op {
+	case "add":
+		fmt.Fprintf(w, "%d + %d = %d", num1, num2, num1+num2)
+	case "sub":
+		fmt.Fprintf(w, "%d - %d = %d", num1, num2, num1-num2)
+	case "mul":
+		fmt.Fprintf(w, "%d * %d = %d", num1, num2, num1*num2)
+	case "div":
+		fmt.Fprintf(w, "%d / %d = %d, remainder = %d", num1, num2, num1/num2, num1%num2)
+	case "gcd":
+		fmt.Fprintf(w, "gcd of %d and %d is %d", num1, num2, gcd(num1, num2))
+	}
+
+}
+
+func main() {
+	port := "12345"
+	if v := os.Getenv("PORT"); len(v) > 0 {
+		port = v
+	}
+	http.HandleFunc("/Lab5", lab5)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
