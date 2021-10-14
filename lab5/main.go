18373ec4b3a84783519ba7eedc9f467646d9fdce
iR150463 <51822951+iR150463@users.noreply.github.com>
2021-10-14 19:01:22 +0800

    ahhhhh

--- lab5/main.go
+++ lab5/main.go
@@ -8,14 +8,64 @@ import (
 	"strconv"
 )
 
+func helloWorld(w http.ResponseWriter) {
+	fmt.Fprintf(w, "hello world!")
+}
+
+func Add(w http.ResponseWriter, num1, num2 int) {
+	fmt.Fprintf(w, "%d + %d = %d", num1, num2, num1+num2)
+}
+
+func Sub(w http.ResponseWriter, num1, num2 int) {
+	fmt.Fprintf(w, "%d - %d = %d", num1, num2, num1-num2)
+}
+
+func Mul(w http.ResponseWriter, num1, num2 int) {
+	fmt.Fprintf(w, "%d * %d = %d", num1, num2, num1*num2)
+}
+
+func Div(w http.ResponseWriter, num1, num2 int) {
+	fmt.Fprintf(w, "%d / %d = %d, remainder = %d", num1, num2, num1/num2, num1%num2)
+}
+
+func GCD(w http.ResponseWriter, num1, num2 int) {
+	a := num1
+	b := num2
+
+	if b > a {
+		a, b = b, a
+	}
+
+	for a != b {
+		a = a - b
+		if b > a {
+			a, b = b, a
+		}
+	}
+
+	fmt.Fprintf(w, "gcd of %d and %d is %d", num1, num2, a)
+}
 
 func lab5(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	op := r.Form["op"][0]
 	num1, _ := strconv.Atoi(r.Form["num1"][0])
 	num2, _ := strconv.Atoi(r.Form["num2"][0])
-	switch op
 
+	switch op {
+	case "Add":
+		Add(w, num1, num2)
+	case "Sub":
+		Sub(w, num1, num2)
+	case "Mul":
+		Mul(w, num1, num2)
+	case "Div":
+		Div(w, num1, num2)
+	case "GCD":
+		GCD(w, num1, num2)
+	default:
+		helloWorld(w)
+	}
 }
 
 func main() {
@@ -23,6 +73,6 @@ func main() {
 	if v := os.Getenv("PORT"); len(v) > 0 {
 		port = v
 	}
-	http.HandleFunc("/Lab5", lab5)
+	http.HandleFunc("/", lab5)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
