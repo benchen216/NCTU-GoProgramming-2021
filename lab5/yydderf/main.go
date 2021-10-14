41515c040031f85135b55c313828f0dddec7d2c5
yydderf <qazwsx900902@gmail.com>
2021-10-14 15:28:43 +0800

    yydderf lab5 attempt1

---
+++ lab5/yydderf/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func add(a, b int) int {
+    return a+b
+}
+
+func sub(a, b int) int {
+    return a-b
+}
+
+func div(a, b int) (int, int) {
+    return a/b, a%b
+}
+
+func mul(a, b int) int {
+    return a*b
+}
+
+func gcd(a, b int) int {
+    c := a % b;
+    if (c == 0) {
+        return b;
+    }
+    a = b;
+    b = c;
+    return gcd(a, b);
+}
+
+func lab5(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	op := r.Form["op"][0]
+	a, _ := strconv.Atoi(r.Form["num1"][0])
+	b, _ := strconv.Atoi(r.Form["num2"][0])
+	switch op {
+    case "add":
+        fmt.Fprintf(w, "%d + %d = %d", a, b, add(a, b));
+    case "sub":
+        fmt.Fprintf(w, "%d - %d = %d", a, b, sub(a, b));
+    case "div":
+        val, remainder := div(a, b);
+        fmt.Fprintf(w, "%d / %d = %d, remainder = %d", a, b, val, remainder);
+    case "mul":
+        fmt.Fprintf(w, "%d * %d = %d", a, b, mul(a, b));
+    case "gcd":
+        fmt.Fprintf(w, "gcd of %d and %d is %d", a, b, gcd(a, b));
+    default:
+        fmt.Fprintf(w, "hello world!")
+    }
+
+}
+
+func main() {
+	port := "12345"
+	if v := os.Getenv("PORT"); len(v) > 0 {
+		port = v
+	}
+	http.HandleFunc("/Lab5", lab5)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
