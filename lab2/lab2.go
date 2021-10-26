dcde5eca79f58c2d873450a345b250ec634f31cf
wschen <person198319@gmail.com>
2021-10-26 22:10:03 +0800

    person198319-lab2-attempt3

--- lab2/lab2.go
+++ lab2/lab2.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
-func Sum() {
+func Sum(n int) int {
+	sum := 0
+	//var i int
+	for i := 1; i <= n; i++ {
+		sum += i
+		if i%7 == 0 {
+			continue
+		}
+		fmt.Print(i)
 
+		if i != n {
+			fmt.Print("+")
+		} else {
+			fmt.Print("=")
+		}
+	}
+	fmt.Println(sum)
+	return sum
 }
 
 func main() {
 	// Please complete the code to make this program be compiled without error.
 	// Notice that you can only add code in this file.
-	var n int64
+	var n int
 	for {
+		fmt.Println("Please input the number n: ")
 		fmt.Scanln(&n)
-
+		if n == 0 {
+			break
+		}
+		Sum(n)
 	}
 }
