93d79bbe7e5f8ba6e20c7e26fa5d5f9032c56199
johnny0234 <johnnychoaka.ee10@nycu.edu.tw>
2021-10-28 15:19:00 +0800

    lab5-0234johnny-attempt1

--- lab5/johnny0234-lab5-attempt1.go
+++ lab5/johnny0234-lab5-attempt1.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	_"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 )
+
 func lab5(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	op := r.Form["op"][0]
@@ -15,7 +15,7 @@ func lab5(w http.ResponseWriter, r *http.Request) {
 	num2, _ := strconv.Atoi(r.Form["num2"][0])
 
 	if op == "gcd" {
-		fmt.Fprintf(w,"gcd of %d and %d is %d",num1,num2, gcd(num1, num2))
+		fmt.Fprintf(w, "gcd of %d and %d is %d", num1, num2, gcd(num1, num2))
 	} else {
 		fmt.Fprintf(w, "error 404")
 	}
@@ -36,3 +36,4 @@ func main() {
 	}
 	http.HandleFunc("/Lab5", lab5)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
