2fd0b0448e68cb2f9cea643341097e4b773b66d3
Connection <69660530+Connection009@users.noreply.github.com>
2021-10-28 15:34:21 +0800

    Update main.go

--- lab6/Connection009/main.go
+++ lab6/Connection009/main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"syscall/js"
 )
 
 func CheckPrime(this js.Value, i []js.Value) interface{} {
-	n := js.Global().Get("value").Get("value").Int()
+	/* add code here */
+	input := js.Global().Get("value").Get("value").String().Int()
 
-	var i int
-	var ans string = "is prime."
-	for i=2; i<n/2; i++ {
-		if n % i == 0 { ans = "is not prime." }
+	if input.ProbablyPrime(0) {
+		js.Global().Get("answer").Set("innerHTML", "is prime")
+	} else {
+		js.Global().Get("answer").Set("innerHTML", "is not prime")
 	}
-
-	js.Global().Get("answer").Set("innerHTML", ans)
-	return js.Global().Get("answer")
+	return nil
 }
 
 func registerCallbacks() {
