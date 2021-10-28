92cb47964184e02bce27a0b69219d5a9af213c2e
Connection <69660530+Connection009@users.noreply.github.com>
2021-10-28 14:37:48 +0800

    Update main.go

--- lab6/Connection009/main.go
+++ lab6/Connection009/main.go
@@ -7,7 +7,8 @@ import (
 
 func CheckPrime(this js.Value, i []js.Value) interface{} {
 	/* add code here */
-	n := js.Global().Get("value").Get("value").Int()
+	n := js.Global().Get("value").Int()
+	fmt.Println("\n\n\n",n,"\n\n\n")
 	var i int
 	var ans string = "is prime."
 	for i=2; i<n/2; i++ {
