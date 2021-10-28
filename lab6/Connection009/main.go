966591a0d580c45146ff34bbd28ae767300757ed
Connection <69660530+Connection009@users.noreply.github.com>
2021-10-28 15:19:36 +0800

    Update main.go

--- lab6/Connection009/main.go
+++ lab6/Connection009/main.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math/big"
 	"syscall/js"
 )
 
 func CheckPrime(this js.Value, i []js.Value) interface{} {
-	/* add code here */
-	input := js.Global().Get("value").Get("value").String()
-	z := new(big.Int)
-	fmt.Sscan(input, z)
-	if z.ProbablyPrime(0) {
-		js.Global().Get("answer").Set("innerHTML", "is prime")
-	} else {
-		js.Global().Get("answer").Set("innerHTML", "is not prime")
+	n := js.Global().Get("value").Get("value").Int()
+
+	var i int
+	var ans string = "is prime."
+	for i=2; i<n/2; i++ {
+		if n % i == 0 { ans = "is not prime." }
 	}
-	return nil
+
+	js.Global().Get("answer").Set("innerHTML", ans)
+	return js.Global().Get("answer")
 }
 
 func registerCallbacks() {
 	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
 }
+
 func main() {
 	fmt.Println("Golang main function executed")
 	registerCallbacks()
+
+	//need block the main thread forever
 	select {}
 }
