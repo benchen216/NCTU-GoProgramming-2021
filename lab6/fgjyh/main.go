ed712520878cfc6b23b4e13398d16dd9368ac5f7
fgjyh <fgjyh1234@gmail.com>
2021-10-28 15:23:34 +0800

    fgjyh-lab6-attempt1

---
+++ lab6/fgjyh/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"syscall/js"
+)
+
+func CheckPrime(this js.Value, i []js.Value) interface{} {
+	z := new(big.Int)
+	z, _ = z.SetString(js.Global().Get("value").Get("value").String(), 10)
+	if z.ProbablyPrime(20) {
+		js.Global().Get("answer").Set("innerHTML", "is prime")
+	} else {
+		js.Global().Get("answer").Set("innerHTML", "is not prime")
+	}
+	return nil
+}
+
+func registerCallbacks() {
+	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
+}
+
+func main() {
+	fmt.Println("Golang main function executed")
+	registerCallbacks()
+
+	//need block the main thread forever
+	select {}
+}
