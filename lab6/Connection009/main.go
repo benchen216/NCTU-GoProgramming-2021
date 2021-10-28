f3a913d80ecdfa6ad5107bc5d2d1ee98c72ef077
Connection <69660530+Connection009@users.noreply.github.com>
2021-10-28 14:58:30 +0800

    Update main.go

--- lab6/Connection009/main.go
+++ lab6/Connection009/main.go
@@ -2,31 +2,32 @@ package main
 
 import (
 	"fmt"
-	"math/big"
 	"strconv"
 	"syscall/js"
 )
 
 func CheckPrime(this js.Value, i []js.Value) interface{} {
 	/* add code here */
-	num, _ := strconv.Atoi(js.Global().Get("value").Get("value").String())
-	var str string
-	if big.NewInt(int64(num)).ProbablyPrime(0) {
-		str = "is prime."
-	} else{
-		str = "is not prime."
+	n, _ := strconv.Atoi(js.Global().Get("value").Get("value").String())
+
+	var i int
+	var ans string = "is prime."
+	for i=2; i<n/2; i++ {
+		if n % i == 0 { ans = "is not prime." }
 	}
-	js.Global().Get("answer").Set("innerHTML", str)
+
+	js.Global().Get("answer").Set("innerHTML", ans)
 	return js.Global().Get("answer")
 }
 
 func registerCallbacks() {
 	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
 }
+
 func main() {
 	fmt.Println("Golang main function executed")
 	registerCallbacks()
 
-	select {}
 	//need block the main thread forever
+	select {}
 }
