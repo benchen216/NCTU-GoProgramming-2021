58b23503f958d8be83b0b3509ae34001cd8eea99
nini302 <raywang.me09@nctu.edu.tw>
2021-10-28 17:29:02 +0800

    attempt1

--- lab6/nini302/main.go
+++ lab6/nini302/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math/big"
+	"strconv"
 	"syscall/js"
 )
 
 func CheckPrime(this js.Value, i []js.Value) interface{} {
 	/* add code here */
+
+	str := js.Global().Get("value").Get("value").String()
+	num, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	if big.NewInt(num).ProbablyPrime(0) {
+		js.Global().Get("answer").Set("innerHTML", "is prime")
+	} else {
+		js.Global().Get("answer").Set("innerHTML", "is not prime")
+	}
+	return nil
 }
 
 func registerCallbacks() {
