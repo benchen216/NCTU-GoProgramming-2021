ce6c74558d7eb4296908de5e762cc770dd5035d6
yydderf <qazwsx900902@gmail.com>
2021-10-28 16:02:19 +0800

    yydderf lab6 attempt1

---
+++ lab6/yydderf/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+    "math/big"
+    "strconv"
+)
+
+func CheckPrime(this js.Value, i []js.Value) interface{} {
+	/* add code here */
+    str := js.Global().Get("value").Get("value").String()
+    num, err := strconv.ParseInt(str, 10, 64)
+    if err != nil {
+        panic(err)
+    }
+    if big.NewInt(num).ProbablyPrime(0) {
+        js.Global().Get("answer").Set("innerHTML", "is prime")
+    } else {
+        js.Global().Get("answer").Set("innerHTML", "is not prime")
+    }
+    return nil
+}
+
+func registerCallbacks() {
+	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
+}
+
+func main() {
+	fmt.Println("Golang main function executed")
+	registerCallbacks()
+
+	//need block the main thread forever
+    c := make(chan bool)
+    <-c
+}
