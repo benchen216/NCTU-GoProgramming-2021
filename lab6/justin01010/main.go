fe0e2d09c899809ea9c881bda80ac2a2f9ad8b4d
justin01010 <justinimbus@yahoo.com.tw>
2021-10-28 17:11:04 +0800

    justin01010-lab6-attempt1

--- lab6/justin01010/main.go
+++ lab6/justin01010/main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+    "strconv"
+    "math/big"
 )
 
 func CheckPrime(this js.Value, i []js.Value) interface{} {
 	/* add code here */
+    str := js.Global().Get("value").Get("value").String()
+    num, err := strconv.ParseInt(str, 10, 64)
+    if err!=nil{
+        panic(err)   //關檔案 印error
+    }
+
+    if big.NewInt(num).ProbablyPrime(0){
+        js.Global().Get("answer").Set("innerHTML","is prime")
+    }else{
+        js.Global().Get("answer").Set("innerHTML","is not prime")
+    }
+    return nil
 }
 
 func registerCallbacks() {
@@ -18,4 +32,5 @@ func main() {
 	registerCallbacks()
 
 	//need block the main thread forever
+    select{}
 }
