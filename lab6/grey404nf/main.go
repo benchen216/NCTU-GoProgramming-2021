b9fadbcfb0559f4c6e3352abed807a31625f7b69
grey68103 <yuan68103@gmail.com>
2021-10-30 02:33:28 +0800

    grey404nf-lab6-attempt1

---
+++ lab6/grey404nf/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+	"strconv"
+	"math/big"
+)
+
+func CheckPrime(this js.Value, i []js.Value) interface{} {
+	/* add code here */
+	
+	str := js.Global().Get("value").Get("value").String()
+	n, _ := strconv.ParseInt(str, 10, 64)
+	
+	if big.NewInt(n).ProbablyPrime(0) {
+		js.Global().Get("answer").Set("innerHTML", "is prime")
+	} else {
+		js.Global().Get("answer").Set("innerHTML", "is not prime")
+	}
+	
+	return nil
+}
+
+func registerCallbacks() {
+	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
+}
+
+func main() {
+	fmt.Println("Golang main function executed")
+	registerCallbacks()
+
+	//need block the main thread forever
+	select{}
+}
