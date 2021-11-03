e44b128a1fb9555a0d852715b1039cb64d0b187f
yeanting <lee.cs06@nycu.edu.tw>
2021-11-04 03:15:51 +0800

    yeanting/lab6

---
+++ lab6/yeanting/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+	"math/big"
+	"strconv"
+)
+
+func CheckPrime(this js.Value, i []js.Value) interface{} {
+	/* add code here */
+	str := js.Global().Get("value").Get("value").String()
+	num, _ := strconv.ParseInt(str, 10, 64)
+	if big.NewInt(num).ProbablyPrime(0) {
+		js.Global().Get("answer").Set("innerHTML","is prime")
+	} else {
+		js.Global().Get("answer").Set("innerHTML","is not prime")
+	}
+	return "nothing"
+}
+
+func registerCallbacks() {
+	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
+}
+
+func main() {
+	fmt.Println("Golang main function executed")
+	registerCallbacks()
+
+	//need block the main thread forever
+	select {}
+}
