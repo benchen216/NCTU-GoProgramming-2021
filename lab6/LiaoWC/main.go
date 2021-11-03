f07e91b3c123fa7ed2253323dee803d00da4b1be
liaoWC <xaviliaoweichen@gmail.com>
2021-11-03 23:16:45 +0800

    Lab6

--- lab6/LiaoWC/main.go
+++ lab6/LiaoWC/main.go
@@ -11,11 +11,13 @@ func CheckPrime(this js.Value, i []js.Value) interface{} {
 	str := js.Global().Get("value").Get("value").String()
 	n := new(big.Int)
 	n, _ = n.SetString(str, 10)
+
 	if n.ProbablyPrime(0) {
 		js.Global().Get("answer").Set("innerHTML", "is prime")
 	} else {
 		js.Global().Get("answer").Set("innerHTML", "is not prime")
 	}
+
 	return nil
 }
 
