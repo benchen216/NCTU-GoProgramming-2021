51b5ed894955d43f26fef9b41e51063cb757bcfc
johnny0234 <johnnychoaka.ee10@nycu.edu.tw>
2021-11-11 14:55:58 +0800

    lab6

---
+++ lab6/johnny0234/johnny0234-lab6-attempt1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"strconv"
+	"syscall/js"
+)
+
+func CheckPrime(this js.Value, i []js.Value) interface{} {
+
+	str := js.Global().Get("value").Get("value").String()
+	number, err := strconv.ParseInt(str, 10, 64)
+	if big.NewInt(number).ProbablyPrime(0) {
+		js.Global().Get("answer").Set("innerHTML", "is prime")
+	} else {
+		js.Global().Get("answer").Set("innerHTML", "is not prime")
+	}
+	return nil
+}
+
+func registerCallbacks() {
+	js.Global().Set("CheckPrime", js.FuncOf(CheckPrime))
+}
+
+func main() {
+	fmt.Println("Golang main function executed")
+	registerCallbacks()
+	select {}
+	//need block the main thread forever
+}
