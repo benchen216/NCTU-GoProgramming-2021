5840a0be27ebe45158aee59b2a11537ce2e00f61
ben <benchen216@gmail.com>
2021-11-18 15:27:19 +0800

    add spec put

--- lab7/validate.sh
+++ lab7/validate.sh
@@ -145,6 +145,23 @@ fi
 
 ans=$(cat <<-END
 {
+    "id": "3",
+    "name": "原子習慣：細微改變帶來巨大成就的實證法則",
+    "pages": "600"
+}
+END
+)
+curl -X PUT -H 'Content-Type: application/json' -d '{"ID":"3","NAME":"原子習慣：細微改變帶來巨大成就的實證法則","PAGES":"600"}' -o result.txt `cat app_url.txt`bookshelf/2
+if [ "$(echo $ans)" != "$(cat result.txt)" ] ; then
+  echo "right ans="$ans
+  echo "your ans=$(cat result.txt)"
+  echo "wrong answer ; NO POINT"
+else
+  echo "GET POINT 1"
+fi
+
+ans=$(cat <<-END
+{
     "message": "book not found"
 }
 END
