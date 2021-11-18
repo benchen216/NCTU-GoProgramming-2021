61bad16b565b6a1c12c6bb7933498bfe7f76caa1
mimiliaogo <mimiliao2000@gmail.com>
2021-11-18 18:12:25 +0800

    lab7

---
+++ lab7/mimiliaogo/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+	// "fmt"
+	// "encoding/json"
+)
+
+type Book struct {
+	// write your own struct
+	Id string `json:"id"`
+	Name string `json:"name"`
+	Pages string `json:"pages"`
+}
+
+var bookshelf = []Book{
+	// init data
+	Book {
+		Id: "1",
+		Name: "Blue Bird",
+		Pages: "500",
+	},
+	
+}
+
+func getBooks(c *gin.Context) {
+	// v_json, _ := json.Marshal(bookshelf)
+	c.JSON(200, bookshelf)
+}
+func getBook(c *gin.Context) {
+	id :=  c.Param("id")
+	for _, v := range bookshelf {
+		if v.Id == id {
+			c.JSON(200, v)
+			return 
+		}
+	}
+	// error handling
+	c.JSON(404, gin.H{
+		"message": "book not found",
+	})
+
+}
+func addBook(c *gin.Context) {
+	newBook := Book{}
+	c.BindJSON(&newBook)
+	// error handling
+	for _, v := range bookshelf {
+		if newBook.Id == v.Id {
+			c.JSON(404, "duplicate book id")
+			return
+		}
+	}
+	
+	bookshelf = append(bookshelf, newBook)
+	c.JSON(200, newBook)
+}
+func deleteBook(c *gin.Context) {
+	id := c.Param("id")
+	for i, v := range bookshelf {
+		if id == v.Id {
+			c.JSON(200, v)
+			bookshelf = append(bookshelf[:i], bookshelf[i+1:]...)
+			return
+		}
+	}
+}
+func main() {
+	r := gin.Default()
+	r.RedirectFixedPath = true
+	r.GET("/bookshelf", getBooks)
+	r.GET("/bookshelf/:id", getBook)
+	r.POST("bookshelf/", addBook)
+	r.DELETE("bookshelf/:id", deleteBook)
+	port := "8080"
+	if v := os.Getenv("PORT"); len(v) > 0 {
+		port = v
+	}
+	r.Run(":" + port)
+}
