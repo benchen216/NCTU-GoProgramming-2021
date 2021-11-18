447437a0280e99cf16d613f94d5b9fcc2b5f6755
ben <benchen216@gmail.com>
2021-11-18 11:01:09 +0800

    upload lab7

---
+++ lab7/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+)
+
+type Book struct {
+	// write your own struct
+}
+
+var bookshelf = []Book{
+	// init data
+}
+
+func getBooks(c *gin.Context) {
+}
+func getBook(c *gin.Context) {
+}
+func addBook(c *gin.Context) {
+}
+func deleteBook(c *gin.Context) {
+}
+func main() {
+	r := gin.Default()
+	r.RedirectFixedPath = true
+	r.GET("/bookshelf", getBooks)
+
+	port := "8080"
+	if v := os.Getenv("PORT"); len(v) > 0 {
+		port = v
+	}
+	r.Run(":" + port)
+}
