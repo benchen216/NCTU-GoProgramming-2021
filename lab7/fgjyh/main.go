fd64c68845a8ec9765ed932b0163306891c91775
fgjyh <fgjyh1234@gmail.com>
2021-11-22 15:33:27 +0800

    fgjyh-lab7-attempt4

--- lab7/fgjyh/main.go
+++ lab7/fgjyh/main.go
@@ -14,7 +14,7 @@ type Book struct {
 }
 
 var bookshelf = []Book{
-	Book{
+	{
 		Id:    "1",
 		Name:  "Blue Bird",
 		Pages: "500",
@@ -33,7 +33,7 @@ func getBook(c *gin.Context) {
 			return
 		}
 	}
-	c.IndentedJSON(404, gin.H{"message": "book not found"})
+	c.IndentedJSON(200, gin.H{"message": "book not found"})
 }
 
 func addBook(c *gin.Context) {
@@ -41,7 +41,7 @@ func addBook(c *gin.Context) {
 	c.BindJSON(&tempBook)
 	for _, s := range bookshelf {
 		if s.Id == tempBook.Id {
-			c.IndentedJSON(404, gin.H{"message": "duplicate book id"})
+			c.IndentedJSON(200, gin.H{"message": "duplicate book id"})
 			return
 		}
 	}
@@ -58,7 +58,7 @@ func deleteBook(c *gin.Context) {
 			return
 		}
 	}
-	c.IndentedJSON(404, gin.H{"message": "book not found"})
+	c.IndentedJSON(200, gin.H{"message": "book not found"})
 }
 
 func modifyBook(c *gin.Context) {
@@ -72,7 +72,7 @@ func modifyBook(c *gin.Context) {
 			return
 		}
 	}
-	c.IndentedJSON(404, gin.H{"message": "book not found"})
+	c.IndentedJSON(200, gin.H{"message": "book not found"})
 }
 
 func main() {
@@ -82,7 +82,7 @@ func main() {
 	r.GET("/bookshelf/:id", getBook)
 	r.POST("/bookshelf", addBook)
 	r.DELETE("/bookshelf/:id", deleteBook)
-	r.PUT("/bookshelf/:id", modifyBook)
+	r.PUT("/bookshelf/*id", modifyBook)
 
 	port := "8080"
 	if v := os.Getenv("PORT"); len(v) > 0 {
