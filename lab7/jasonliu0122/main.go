e18174a97ba7302e96eef7030662cdfa48897056
jason liu <jasonliu0122@icloud.com>
2021-11-24 13:36:41 +0800

    jasonliu0122-lab7-attempt1

---
+++ lab7/jasonliu0122/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+)
+
+type Book struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Pages string `json:"pages"`
+}
+
+var bookshelf = []Book{
+	// init data
+	{
+		ID		: "1",
+		Name	: "Blue Bird",
+		Pages	: "500",
+	},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, bookshelf)
+}
+func getBook(c *gin.Context) {
+	id := c.Param("id")
+
+	for i := range bookshelf {
+		if bookshelf[i].ID == id {
+			c.IndentedJSON(http.StatusOK, bookshelf[i])
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+}
+func addBook(c *gin.Context) {
+	var book Book
+	c.BindJSON(&book)
+
+	for i := range bookshelf {
+		if bookshelf[i].ID == book.ID {
+			c.IndentedJSON(http.StatusNotFound, gin.H{"message": "duplicate book id"})
+			return
+		}
+	}
+
+	bookshelf = append(bookshelf, book)
+	c.IndentedJSON(http.StatusOK, book)
+}
+func deleteBook(c *gin.Context) {
+	id := c.Param("id")
+
+	for i := range bookshelf {
+		if bookshelf[i].ID == id {
+			c.IndentedJSON(http.StatusOK, bookshelf[i])
+			bookshelf = append(bookshelf[:i],bookshelf[i+1:]...)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "book not found"})
+
+
+}
+func updateBook(c *gin.Context) {
+	var book Book
+	id := c.Param("id")
+	c.BindJSON(&book)
+
+	for i := range bookshelf {
+		if bookshelf[i].ID == id {	
+			bookshelf[i] = book
+			c.IndentedJSON(200, updateBook)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+
+}
+func main() {
+	r := gin.Default()
+	r.RedirectFixedPath = true
+	r.GET("/bookshelf", getBooks)
+	r.GET("/bookshelf/:id", getBook)
+	r.POST("/bookshelf/", addBook);
+	r.DELETE("/bookshelf/:id", deleteBook)
+	r.PUT("/bookshelf/:id", updateBook)
+
+
+
+	port := "8080"
+	if v := os.Getenv("PORT"); len(v) > 0 {
+		port = v
+	}
+	r.Run(":" + port)
+}
