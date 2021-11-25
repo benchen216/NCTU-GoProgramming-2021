4d18f30968770457ce4008cd2dd997f3dc73e948
andytsai <andytsai2000@gmail.com>
2021-11-25 12:29:11 +0800

    getBooks

--- lab7/andytsai2000/main.go
+++ lab7/andytsai2000/main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Book struct {
 	// write your own struct
+	id     string `json:"id"`
+	title  string `json:"title"`
+	author string `json:"author"`
 }
 
 var bookshelf = []Book{
 	// init data
+	{
+		id:     "0001",
+		title:  "aa bird",
+		author: "cat",
+	},
+	{
+		id:     "0002",
+		title:  "aa bird 2",
+		author: "cat 2",
+	},
+	{
+		id:     "0003",
+		title:  "aa bird 3",
+		author: "cat 3",
+	},
+	{
+		id:     "0004",
+		title:  "aa bird 4",
+		author: "cat 4",
+	},
+	{
+		id:     "0005",
+		title:  "aa bird 5",
+		author: "cat 5",
+	},
 }
 
 func getBooks(c *gin.Context) {
+	c.IntentedJSON(http.StatusOK, bookshelf)
 }
 func getBook(c *gin.Context) {
 }
