8d32e99fb1c3944440dbcf7ae6c6445eefea980b
fgjyh <fgjyh1234@gmail.com>
2021-12-03 12:17:03 +0800

    fgjyh-lab9-attempt2

--- lab9/fgjyh/lab9.go
+++ lab9/fgjyh/lab9.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	// [TODO] set your module name, go mod init my_module->MOUDULE_NAME
 	"fgjyh/cw"
 )
 
 func main() {
 	cwSystem := cw.System{}
-	fmt.Println(cwSystem)
+	cwSystem.String()
 }
