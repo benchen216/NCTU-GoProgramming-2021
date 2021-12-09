3f0a5010b55fb3c80322de895e1a7401126e21d8
jason liu <jasonliu0122@icloud.com>
2021-12-09 15:53:32 +0800

    jasonliu0122-lab10-attempt1

---
+++ lab10/jasonliu0122/lab10.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var doorStatus string
+var handStatus string
+var lock sync.Mutex
+
+func hand() {
+	lock.Lock()
+	handStatus = "in"
+	time.Sleep(time.Millisecond * 200)
+	handStatus = "out"
+	lock.Unlock()
+	wg.Done()
+
+}
+
+func door() {
+	lock.Lock()
+	doorStatus = "close"
+	time.Sleep(time.Millisecond * 200)
+	if handStatus == "in" {
+		fmt.Println("夾到手了啦！")
+	} else {
+		fmt.Println("沒夾到喔！")
+	}
+	doorStatus = "open"
+	lock.Unlock()
+	wg.Done()
+}
+
+var wg sync.WaitGroup
+
+func main() {
+	for i := 0; i < 50; i++ {
+		wg.Add(2)
+		go door()
+		go hand()
+		wg.Wait()
+		time.Sleep(time.Millisecond * 200)
+	}
+}
