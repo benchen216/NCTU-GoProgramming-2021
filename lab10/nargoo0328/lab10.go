f0bd74177640eaaf91aa3189c35f43b396089a54
0612234 <tomy45651@gmail.com>
2021-12-09 21:34:16 +0800

    nargoo0328-lab10-attempt1

---
+++ lab10/nargoo0328/lab10.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var doorStatus string
+var handStatus string
+
+func hand() {
+	mu.Lock()
+	handStatus = "in"
+	time.Sleep(time.Millisecond * 200)
+	handStatus = "out"
+	mu.Unlock()
+	wg.Done()
+}
+
+func door() {
+	mu.Lock()
+	doorStatus = "close"
+	time.Sleep(time.Millisecond * 200)
+	if handStatus == "in" {
+		fmt.Println("夾到手了啦！")
+	} else {
+		fmt.Println("沒夾到喔！")
+	}
+	doorStatus = "open"
+	mu.Unlock()
+	wg.Done()
+}
+
+var wg sync.WaitGroup
+var mu sync.Mutex
+
+func main() {
+	for i := 0; i < 50; i++ {
+		wg.Add(2)
+		go door()
+		go hand()
+		wg.Wait()
+		time.Sleep(time.Millisecond * 200)
+	}
+}
