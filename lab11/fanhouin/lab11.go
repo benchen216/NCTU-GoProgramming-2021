eddc482a5d8e185eeb3ec7b70c6b486afdfdb32f
Fan <fanhouin@gmail.com>
2021-12-10 18:37:36 +0800

    sample test2

--- lab11/fanhouin/lab11.go
+++ lab11/fanhouin/lab11.go
@@ -13,7 +13,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	pinger.SetPrivileged(true)
+	// pinger.SetPrivileged(true)
 	// Listen for Ctrl-C.
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
