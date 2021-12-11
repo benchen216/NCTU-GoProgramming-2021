96b2c2cf5abc0982325a658a9600798aeb33ed3e
Fan <fanhouin@gmail.com>
2021-12-11 16:56:28 +0800

    lab11

---
+++ lab11/YourGithubUsername/lab11.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+func init() {
+	// Define the other flags here
+	// flag.IntVar()
+}
+
+func main() {
+	flag.Parse()
+	// flag.PrintDefaults()
+
+	c := colly.NewCollector()
+
+	c.OnHTML("div#topbar.bbs-content > a#logo", func(e *colly.HTMLElement) {
+		fmt.Println("demo1: " + e.Text)
+	})
+
+	c.OnHTML("div[class='bbs-content'] > a[id='logo']", func(e *colly.HTMLElement) {
+		fmt.Println("demo2: " + e.Text)
+	})
+
+	c.OnHTML(".f2", func(e *colly.HTMLElement) {
+		fmt.Println("demo3: " + e.Text)
+	})
+
+	c.OnHTML("#logo", func(e *colly.HTMLElement) {
+		fmt.Println("demo4: " + e.Attr("href"))
+	})
+
+	c.Visit("https://www.ptt.cc/bbs/Stock/M.1610102078.A.16E.html")
+	// c.Visit("https://www.csie.ntu.edu.tw/members/teacher.php?mclass1=110")
+	c.Wait()
+}
