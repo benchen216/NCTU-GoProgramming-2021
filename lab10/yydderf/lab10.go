1a6ad28878b73f09867a12fc5aece086474b3b4b
yydderf <qazwsx900902@gmail.com>
2021-12-12 00:45:11 +0800

    yydderf lab10 attempt1

--- lab10/yydderf/lab10.go
+++ lab10/yydderf/lab10.go
@@ -10,25 +10,30 @@ var doorStatus string
 var handStatus string
 
 func hand() {
+    mu.Lock()
 	handStatus = "in"
 	time.Sleep(time.Millisecond * 200)
+    mu.Unlock()
 	handStatus = "out"
 	wg.Done()
 }
 
 func door() {
 	doorStatus = "close"
+    mu.Lock()
 	time.Sleep(time.Millisecond * 200)
 	if handStatus == "in" {
 		fmt.Println("夾到手了啦！")
 	} else {
 		fmt.Println("沒夾到喔！")
 	}
+    mu.Unlock()
 	doorStatus = "open"
 	wg.Done()
 }
 
 var wg sync.WaitGroup
+var mu sync.Mutex
 
 func main() {
 	for i := 0; i < 50; i++ {
