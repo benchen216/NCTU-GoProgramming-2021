0899d0598847364d229fef8e80591654cac4e14e
justin01010 <justinimbus@yahoo.com.tw>
2021-12-17 21:24:34 +0800

    lab11

---
+++ lab11/justin01010/lab11.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+var (
+	max int
+	webpage string
+)
+
+func init() {
+	flag.IntVar(&max, "max", 10, "Max Printing")
+	flag.StringVar(&webpage, "w", "ptt", "Web page")
+}
+
+
+func ppt(c *colly.Collector) {
+	c.OnHTML("div#main-content", func(e *colly.HTMLElement) {
+		e.ForEach("div.push", func(i int, e *colly.HTMLElement) {
+			if i >= max {
+				return
+			}
+			fmt.Printf("%d. 名字: %s, ", i + 1, e.ChildText(".push-userid"))
+			fmt.Printf("留言%s, ", e.ChildText(".push-content"))
+			fmt.Printf("時間: %s\n", e.ChildText(".push-ipdatetime"))
+		})
+	})
+}
+
+func ncku(c *colly.Collector) {
+	c.OnHTML("div#tab1", func(h *colly.HTMLElement) {
+		h.ForEach("td.teacherInfo", func(i int, h *colly.HTMLElement) {
+			if i >= max {
+				return
+			}
+			fmt.Printf("%d. 姓名: %s, ", i + 1, h.ChildText("span.content_title2 > a"))
+
+			link := h.ChildAttr("p.infop > a", "href")
+			if link == "" {
+				link = "NULL"
+			}
+			fmt.Printf("網站: %s\n", link)
+		})
+	})
+}
+
+func main() {
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		flag.PrintDefaults()
+	}
+	c := colly.NewCollector()
+
+	if webpage == "ncku" {
+		ncku(c)
+		c.Visit("https://www.csie.ncku.edu.tw/ncku_csie/depmember/teacher")
+	} else {
+		ppt(c)
+        c.Visit("https://www.ptt.cc/bbs/Stock/M.1610102078.A.16E.html")
+	}
+
+	c.Wait()
+}
