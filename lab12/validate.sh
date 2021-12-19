3d2b02e6f2218ee481be9c56fafa00cfaa5450cf
a13579and2468 <a13579and2468@a13579and2468-X555LD>
2021-12-19 22:44:45 +0800

    hihi

--- lab12/validate.sh
+++ lab12/validate.sh
@@ -14,14 +14,18 @@ pwd
 ls
 chromium --version
 chromedriver --version
+chmod 777 static
+chmom 777 static/index.html
 go run lab12.go command >out 2>&1 &
-curl --retry-connrefused --retry 4 --connect-timeout 5 http://0.0.0.0:8899 --verbose
+curl --retry-connrefused --retry 4 --connect-timeout 5 http://0.0.0.0:8899/ --verbose
 echo "wget"
 wget http://0.0.0.0:8899/index.html
 echo "cat index.html"
 cat index.html
 echo "cat out"
 cat out
+ls -al
+ls -al static
 python3  $solution_path/../validate.py
 
 echo "deleting working directory $tmp_dir"
