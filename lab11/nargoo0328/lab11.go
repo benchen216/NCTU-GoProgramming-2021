92c4895f45610540bcbd6d082382d0b6694164e3
0612234 <tomy45651@gmail.com>
2021-12-22 02:20:10 +0800

    nargoo0328-lab11-attempt1

---
+++ lab11/nargoo0328/lab11.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+var max_output int
+var which_article string
+
+func init() {
+	// Define the other flags here
+	flag.IntVar(&max_output, "max", 10, "Max Printing")
+	flag.StringVar(&which_article, "w", "ptt", "Web page")
+}
+
+func main() {
+	flag.Parse()
+	if flag.NFlag()==0{
+		flag.PrintDefaults()
+	}else{
+		c := colly.NewCollector()
+		index :=1
+		if which_article=="ptt"{
+			c.OnHTML(".push", func(e *colly.HTMLElement) {
+				if index>max_output{
+					return
+				}
+				id := e.ChildText(".push-userid")
+				content := e.ChildText(".push-content")
+				time := e.ChildText(".push-ipdatetime")
+				fmt.Printf("%d. 名字: %s, 留言%s, 時間: %s\n",index,id,content,time)
+				index++
+			})
+			c.Visit("https://www.ptt.cc/bbs/Stock/M.1610102078.A.16E.html")
+		}else{
+			c.OnHTML(".teacherInfo", func(e *colly.HTMLElement) {
+				if index>max_output{
+					return
+				}
+				name := e.ChildText(".content_title2")
+				temp := e.ChildAttrs("a","href")
+				url := "NULL"
+				if len(temp)>=2 && temp[1]!=""{
+					url = temp[1]
+				}
+				fmt.Printf("%d. 姓名: %s, 網站: %s\n",index,name,url)
+				index++
+			})
+			c.Visit("https://www.csie.ncku.edu.tw/ncku_csie/depmember/teacher")
+		}
+		c.Wait()
+	}
+}
