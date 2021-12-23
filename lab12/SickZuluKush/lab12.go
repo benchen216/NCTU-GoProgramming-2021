fd4e4603ae8b3feb1e48723afe4f5779f98b3e21
SickZuluKush <64488717+SickZuluKush@users.noreply.github.com>
2021-12-23 17:28:08 +0900

    SickZuluKush lab12 attempt 1

---
+++ lab12/SickZuluKush/lab12.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"context"
+	"strings"
+	"os"
+	"fmt"
+	"bufio"
+
+	"github.com/gorilla/websocket"
+	"github.com/reactivex/rxgo/v2"
+)
+
+type client chan<- string // an outgoing message channel
+
+var (
+	entering      = make(chan client)
+	leaving       = make(chan client)
+	messages      = make(chan rxgo.Item) // all incoming client messages
+	ObservableMsg = rxgo.FromChannel(messages)
+	forbidden []string
+	sensitive []string
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	MessageBoardcast := ObservableMsg.Observe()
+	for {
+		select {
+		case msg := <-MessageBoardcast:
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli <- msg.V.(string)
+			}
+
+		case cli := <-entering:
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+//clientWriter and main
+func clientWriter(conn *websocket.Conn, ch <-chan string) {
+	for msg := range ch {
+		conn.WriteMessage(1, []byte(msg))
+	}
+}
+
+func wshandle(w http.ResponseWriter, r *http.Request) {
+	upgrader := &websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		return
+	}
+
+	ch := make(chan string) // outgoing client messages
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "你是 " + who + "\n"
+	messages <- rxgo.Of(who + " 來到了現場" + "\n")
+	entering <- ch
+
+	defer func() {
+		log.Println("disconnect !!")
+		leaving <- ch
+		messages <- rxgo.Of(who + " 離開了" + "\n")
+		conn.Close()
+	}()
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		messages <- rxgo.Of(who + " 表示: " + string(msg))
+	}
+}
+
+func InitObservable() {
+	ObservableMsg = ObservableMsg.Filter(func(item interface{}) bool {
+			s := strings.SplitN(item.(string), "表示: ", 2)
+			
+			if len(s) == 2 {
+				filter, _ := have_word(s[1], forbidden) 
+				return !filter
+			}
+			
+			return true
+	}).Map(func(_ context.Context, item interface{}) (interface{}, error) {
+			s := strings.SplitN(item.(string), "表示: ", 2)
+			
+			if len(s) == 2 {
+				var replace []rune
+				
+				for filter, word := have_word(s[1], sensitive); filter == true; {
+					replace = []rune(word)
+					replace[1] = '*'
+					s[1] = strings.ReplaceAll(s[1], word, string(replace))
+					
+					filter, word = have_word(s[1], sensitive)
+				}
+				
+				return s[0] + "表示: " + s[1], nil
+			}
+			
+			return item.(string), nil
+	})
+	
+}
+
+func load_filter(filename string, container *[]string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	
+	defer file.Close()
+	
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		*container = append(*container, scanner.Text())
+	}
+	
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func have_word(target string, words []string) (bool, string) {
+	for _, x := range(words) {
+		if strings.Contains(target, x) {
+			return true, x
+		}
+	}
+	
+	return false, ""
+} 
+
+func main() {
+	load_filter("dirtytalk.txt", &forbidden)
+	load_filter("sensitive_name.txt", &sensitive)
+	
+	InitObservable()
+	go broadcaster()
+	http.HandleFunc("/wschatroom", wshandle)
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	log.Println("server start at :8899")
+	log.Fatal(http.ListenAndServe(":8899", nil))
+}
