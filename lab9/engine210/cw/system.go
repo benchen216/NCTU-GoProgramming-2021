9b2b96cdc555ffcfb36a0e1632af4a50b181b84a
Stevn Wang <engine210@gmail.com>
2021-12-27 20:25:42 +0800

    something about lab9

--- lab9/engine210/cw/system.go
+++ lab9/engine210/cw/system.go
@@ -14,28 +14,35 @@ func checkErr(e error) {
 
 type System struct {
 	// you can add some data type if you like
+	ptt_articles PTTArticles
+	fb_articles  FBArticles
 }
 
 func (System) String() string {
 	return "There's nothing here."
 }
 
-func (System) LoadPTT(url string) PTTArticles {
+func (s *System) LoadPTT(url string) PTTArticles {
 	var articles PTTArticles
 	jsonBlob, _ := ioutil.ReadFile(url)
 	checkErr(json.Unmarshal(jsonBlob, &articles))
+	s.ptt_articles = articles
 	return articles
 }
 
-func (System) LoadFB(url string) FBArticles {
+func (s *System) LoadFB(url string) FBArticles {
 	var articles FBArticles
 	jsonBlob, _ := ioutil.ReadFile(url)
 	checkErr(json.Unmarshal(jsonBlob, &articles))
+	s.fb_articles = articles
 	return articles
 }
 
-func (System) CountCyberWarriors() {
-
+func (s *System) CountCyberWarriors() {
+	fmt.Printf("%s\n", s.ptt_articles.Articles[0].Author)
+	// for i, a := range articles {
+	// 	fmt.Printf("%+v\n", a)
+	// }
 }
 
 /* [TODO]
--- lab9/engine210/lab9.go
+++ lab9/engine210/lab9.go
@@ -13,6 +13,8 @@ func main() {
 	PTTArticles := cwSystem.LoadPTT("../data/ptt.json")
 	fmt.Printf("%+v\n", PTTArticles.Articles[0])
 
+	cwSystem.CountCyberWarriors()
+
 	FBArticles := cwSystem.LoadFB("../data/fb.json")
 	fmt.Printf("%+v\n", FBArticles.Articles[0])
 
