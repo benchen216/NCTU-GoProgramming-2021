7b449eeef18b2d688e57cb71f334b6d7e371d6e0
Stevn Wang <engine210@gmail.com>
2021-12-28 22:04:23 +0800

    finish lab9 attempt1

--- lab9/engine210/cw/system.go
+++ lab9/engine210/cw/system.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"sort"
+	"strings"
 )
 
 func checkErr(e error) {
@@ -38,15 +40,84 @@ func (s *System) LoadFB(url string) FBArticles {
 	return articles
 }
 
-func (s *System) CountCyberWarriors() {
-	fmt.Printf("%s\n", s.ptt_articles.Articles[0].Author)
-	// for i, a := range articles {
-	// 	fmt.Printf("%+v\n", a)
-	// }
+func (s *System) CountCyberWarriors(ip_user_num int) {
+	// fmt.Printf("balballba%s\n", s.ptt_articles.Articles[0].Author)
+	// fmt.Printf("balballba%s\n", s.ptt_articles.Articles[0].Ip)
+
+	ip_map := make(map[string]map[string]struct{})
+
+	for _, v := range s.ptt_articles.Articles {
+		_, found := ip_map[v.Ip]
+		if found {
+			ip_map[v.Ip][v.Author] = struct{}{}
+		} else {
+			ip_map[v.Ip] = make(map[string]struct{})
+			ip_map[v.Ip][v.Author] = struct{}{}
+		}
+	}
+	var keys []string
+	for k, _ := range ip_map {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		v := ip_map[k]
+		if len(v) > ip_user_num && k != "None" {
+			fmt.Printf("%s, total: %d\n", k, len(v))
+			var l []string
+			for kk, _ := range v {
+				l = append(l, kk)
+			}
+			sort.Strings(l)
+			output := "[" + strings.Join(l, `, `) + `]`
+			fmt.Println(output)
+		}
+	}
 }
 
-/* [TODO]
-func (System) CountKeyWord(){
+func (s *System) CountKeyWord(keyword_count int, keywords []string) {
+	keyword_map := make(map[string]map[string]int) //map[keyword]map[author]count
+	for _, keyword := range keywords {
+		keyword_map[keyword] = make(map[string]int)
+	}
 
+	for _, v := range s.ptt_articles.Articles {
+		for _, keyword := range keywords {
+			if strings.Contains(v.Article_title, keyword) {
+				_, found := keyword_map[keyword][v.Author]
+				if found {
+					keyword_map[keyword][v.Author] += 1
+				} else {
+					keyword_map[keyword][v.Author] = 1
+				}
+			}
+		}
+	}
+	for _, v := range s.fb_articles.Articles {
+		for _, keyword := range keywords {
+			if strings.Contains(v.Article_title, keyword) {
+				_, found := keyword_map[keyword][v.Author]
+				if found {
+					keyword_map[keyword][v.Author] += 1
+				} else {
+					keyword_map[keyword][v.Author] = 1
+				}
+			}
+		}
+	}
+
+	for k, v := range keyword_map {
+		fmt.Printf("%s, ", k)
+		var l []string
+		for kk, vv := range v {
+			if vv > keyword_count {
+				l = append(l, kk)
+			}
+		}
+		fmt.Printf("total: %d\n", len(l))
+		sort.Strings(l)
+		output := "[" + strings.Join(l, `, `) + `]`
+		fmt.Printf("%s\n", output)
+	}
 }
-*/
--- lab9/engine210/lab9.go
+++ lab9/engine210/lab9.go
@@ -1,22 +1,35 @@
 package main
 
 import (
-	"fmt"
-
 	// [TODO] set your module name, go mod init my_module->MOUDULE_NAME
+	"fmt"
 	"lab9/cw"
+	"os"
+	"strconv"
 )
 
 func main() {
 	cwSystem := cw.System{}
 
-	PTTArticles := cwSystem.LoadPTT("../data/ptt.json")
-	fmt.Printf("%+v\n", PTTArticles.Articles[0])
+	cwSystem.LoadPTT("../data/ptt.json")
+	// PTTArticles := cwSystem.LoadPTT("../data/ptt.json")
+	// fmt.Printf("%+v\n", PTTArticles.Articles[0])
 
-	cwSystem.CountCyberWarriors()
+	cwSystem.LoadFB("../data/fb.json")
+	// FBArticles := cwSystem.LoadFB("../data/fb.json")
+	// fmt.Printf("%+v\n", FBArticles.Articles[0])
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run lab9.go <IP_USER_NUM> <KEYWORD_COUNT> <KEYWORD...>")
+		os.Exit(1)
+	}
 
-	FBArticles := cwSystem.LoadFB("../data/fb.json")
-	fmt.Printf("%+v\n", FBArticles.Articles[0])
+	ip_user_num, _ := strconv.Atoi(os.Args[1])
+	cwSystem.CountCyberWarriors(ip_user_num)
 
-	fmt.Println(cwSystem)
+	keywords := os.Args[3:]
+	// st := []string{"蔡英文", "韓國瑜", "宋楚瑜"}
+	keyword_count, _ := strconv.Atoi(os.Args[2])
+	cwSystem.CountKeyWord(keyword_count, keywords)
+
+	// fmt.Println(cwSystem)
 }
