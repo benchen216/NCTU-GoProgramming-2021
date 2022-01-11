006e4d3fc2e064ae5df6196daba1082c05c61604
andytsai <andytsai2000@gmail.com>
2022-01-11 15:52:37 +0800

    submit

--- lab11/andytsai2000/lab11.go
+++ lab11/andytsai2000/lab11.go
@@ -3,38 +3,67 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
 
 	"github.com/gocolly/colly"
 )
 
+//flags
+var (
+	max int
+	web string
+)
+
 func init() {
-	// Define the other flags here
-	// flag.IntVar()
+	flag.IntVar(&max, "max", 10, "Max Printing")
+	flag.StringVar(&web, "w", "ptt", "Web page")
+}
+
+func ptt(c *colly.Collector) {
+	c.OnHTML("div[id='main-content']", func(e *colly.HTMLElement) {
+		e.ForEach("div[class='push']", func(i int, e *colly.HTMLElement) {
+			if i < max {
+				fmt.Printf(strconv.Itoa(i+1) + ". 名字: " + e.ChildText(".push-userid") +
+					", 留言" + e.ChildText(".push-content") +
+					", 時間: " + e.ChildText(".push-ipdatetime") + "\n")
+			}
+		})
+	})
+
+}
+
+func ncku(c *colly.Collector) {
+	c.OnHTML("div[class='content_maintext tab-content'] > div[id='tab1'] > table[class='teacherTableStyle']", func(e *colly.HTMLElement) {
+		e.ForEach("td[class='teacherInfo']", func(i int, e *colly.HTMLElement) {
+			if i < max {
+				if len(e.ChildAttr("p[class='infop'] > a", "href")) == 0 {
+					fmt.Printf(strconv.Itoa(i+1) + ". 姓名: " + e.ChildText("span[class='content_title2'] > a") + ", 網站: NULL\n")
+				} else {
+					fmt.Printf(strconv.Itoa(i+1) + ". 姓名: " + e.ChildText("span[class='content_title2'] > a") + ", 網站: " + e.ChildAttr("p[class='infop'] > a", "href") + "\n")
+				}
+			}
+
+		})
+	})
+
 }
 
 func main() {
 	flag.Parse()
-	// flag.PrintDefaults()
+	if flag.NFlag() == 0 {
+		flag.PrintDefaults()
+		return
+	}
 
 	c := colly.NewCollector()
 
-	c.OnHTML("div#topbar.bbs-content > a#logo", func(e *colly.HTMLElement) {
-		fmt.Println("demo1: " + e.Text)
-	})
+	if web == "ptt" {
+		ptt(c)
+		c.Visit("https://www.ptt.cc/bbs/Stock/M.1610102078.A.16E.html")
+	} else {
+		ncku(c)
+		c.Visit("https://www.csie.ncku.edu.tw/ncku_csie/depmember/teacher")
+	}
 
-	c.OnHTML("div[class='bbs-content'] > a[id='logo']", func(e *colly.HTMLElement) {
-		fmt.Println("demo2: " + e.Text)
-	})
-
-	c.OnHTML(".f2", func(e *colly.HTMLElement) {
-		fmt.Println("demo3: " + e.Text)
-	})
-
-	c.OnHTML("#logo", func(e *colly.HTMLElement) {
-		fmt.Println("demo4: " + e.Attr("href"))
-	})
-
-	c.Visit("https://www.ptt.cc/bbs/Stock/M.1610102078.A.16E.html")
-	// c.Visit("https://www.csie.ncku.edu.tw/ncku_csie/depmember/teacher")
 	c.Wait()
 }
