a9901e8ccba453ee16d27dc60e6108ffc6bf9600
wschen <person198319@gmail.com>
2022-01-18 11:39:24 +0800

    person198319-lab4-attempt2

--- lab4/person198319/main.go
+++ lab4/person198319/main.go
@@ -9,61 +9,58 @@ import (
 	"strings"
 )
 
-func add(a, b int) int {
-	return a + b
-}
-
-func sub(a, b int) int {
-	return a - b
-}
-
-func mul(a, b int) int {
-	return a * b
-}
-
-func div(a, b int) int {
-	return a / b
-}
-
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world!")
 
 	pathParts := strings.SplitN(r.URL.Path, "/", -1)
 
-	operation := pathParts[0]
-	a, _ := strconv.Atoi(pathParts[1])
-	b, _ := strconv.Atoi(pathParts[2])
-	var c int
+	if len(pathParts) <= 3 {
+		fmt.Fprintf(w, "hello world!")
+		return
+	}
+
+	operation := pathParts[1]
+	a, _ := strconv.Atoi(pathParts[2])
+	b, _ := strconv.Atoi(pathParts[3])
 	if operation == "add" {
-		c = add(a, b)
-		x := "+"
-		y := "="
-		fmt.Fprint(w, a, x, b, y, c)
+		fmt.Fprintf(w, "%d + %d = %d", a, b, Add(a, b))
+
 	} else if operation == "sub" {
-		c = sub(a, b)
-		x := "-"
-		y := "="
-		fmt.Fprint(w, a, x, b, y, c)
+		fmt.Fprintf(w, "%d - %d = %d", a, b, Sub(a, b))
+
 	} else if operation == "mul" {
-		c = mul(a, b)
-		x := "*"
-		y := "="
-		fmt.Fprint(w, a, x, b, y, c)
+		fmt.Fprintf(w, "%d * %d = %d", a, b, Mul(a, b))
+
 	} else if operation == "div" {
-		c = div(a, b)
-		x := "/"
-		y := "="
-		fmt.Fprint(w, a, x, b, y, c)
+		fmt.Fprintf(w, "%d / %d = %d, remainder = %d", a, b, a/b, a%b)
+
 	} else {
 		fmt.Fprintf(w, "hello world!")
 	}
 }
 
+func Add(x, y int) int {
+	return x + y
+}
+
+func Sub(x, y int) int {
+	return x - y
+}
+
+func Mul(x, y int) int {
+	return x * y
+}
+
+func Div(x, y int) int {
+	return x / y
+}
+
 func main() {
+
 	port := "12345"
 	if v := os.Getenv("PORT"); len(v) > 0 {
 		port = v
 	}
+
 	http.HandleFunc("/", hello)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
