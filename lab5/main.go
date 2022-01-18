99b9b183e572c025048b92969337f3cbddfccf2a
wschen <person198319@gmail.com>
2022-01-18 16:31:48 +0800

    person198319-lab5-attempt1

--- lab5/main.go
+++ lab5/main.go
@@ -8,13 +8,55 @@ import (
 	"strconv"
 )
 
+func gcd(a int, b int) int {
 
-func lab5(w http.ResponseWriter, r *http.Request) {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+
+	return a
+}
+
+func lab(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
+
 	op := r.Form["op"][0]
 	num1, _ := strconv.Atoi(r.Form["num1"][0])
 	num2, _ := strconv.Atoi(r.Form["num2"][0])
-	switch op
+
+	switch op {
+	case "add":
+		result := num1 + num2
+		output := strconv.Itoa(num1) + " + " + strconv.Itoa(num2) + " = " + strconv.Itoa(result)
+		fmt.Fprintf(w, output)
+
+	case "sub":
+		result := num1 - num2
+		output := strconv.Itoa(num1) + " - " + strconv.Itoa(num2) + " = " + strconv.Itoa(result)
+		fmt.Fprintf(w, output)
+
+	case "mul":
+		result := num1 * num2
+		output := strconv.Itoa(num1) + " * " + strconv.Itoa(num2) + " = " + strconv.Itoa(result)
+		fmt.Fprintf(w, output)
+
+	case "div":
+		result := num1 / num2
+		output := strconv.Itoa(num1) + " / " + strconv.Itoa(num2) + " = " + strconv.Itoa(result)
+		output += ", remainder = " + strconv.Itoa(num1%num2)
+		fmt.Fprintf(w, output)
+
+	case "gcd":
+		result := gcd(num1, num2)
+		output := "gcd of " + strconv.Itoa(num1) + " and " + strconv.Itoa(num2) + " is " + strconv.Itoa(result)
+		fmt.Fprintf(w, output)
+
+	default:
+		fmt.Fprintf(w, "Hello World")
+
+	}
 
 }
 
@@ -23,6 +65,6 @@ func main() {
 	if v := os.Getenv("PORT"); len(v) > 0 {
 		port = v
 	}
-	http.HandleFunc("/Lab5", lab5)
+	http.HandleFunc("/", lab)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
