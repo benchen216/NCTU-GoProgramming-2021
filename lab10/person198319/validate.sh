b7b8760304de7f67707b8c6a3c537cd2631b776e
wschen <person198319@gmail.com>
2022-01-19 11:25:42 +0800

    person198319-lab10-attempt1

---
+++ lab10/person198319/validate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+test_path="${BASH_SOURCE[0]}"
+solution_path="$(realpath .)"
+tmp_dir=$(mktemp -d -t lab10-XXXXXXXXXX)
+
+echo "working directory: $tmp_dir"
+cd $tmp_dir
+
+#rm -rf *
+cp $solution_path/lab10.go .
+result=$(go run lab10.go 2>&1) ; ret=$?
+if [ $ret -ne 0 ] ; then
+  echo "\"go run lab10.go\" fails ; NO POINT"
+else
+  echo "\"go run lab10.go\" output: \"$result\""
+  ans=$(python3 -c 'print("沒夾到喔！\n"*50)')
+  if [ "$ans" != "$result" ] ; then
+    echo "wrong answer ; NO POINT"
+    echo "答案錯了"
+    echo "你的輸出:"
+    echo $result
+    echo "正確答案:"
+    echo $ans
+    exit 1
+  else
+    echo "GET POINT 1"
+    echo "答案正確"
+  fi
+fi
+
+echo "deleting working directory $tmp_dir"
+rm -rf $tmp_dir
+
+exit 0
