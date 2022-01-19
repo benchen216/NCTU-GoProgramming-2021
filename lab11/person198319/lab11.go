42d82d4df91ceaeab990aae52c27becf3f8cad81
wschen <person198319@gmail.com>
2022-01-19 12:18:51 +0800

    person198319-lab11-attempt1

---
+++ lab11/person198319/lab11.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+var output_max int
+var which_article string
+
+func init() {
+	// Define the other flags here
+	// flag.IntVar()
+	flag.IntVar(&output_max, "max", 10, "Max Printing")
+	flag.StringVar(&which_article, "w", "ptt", "Web page")
+}
+
+func main() {
+	flag.Parse()
+	// flag.PrintDefaults()
+	if flag.NFlag() == 0 {
+		flag.PrintDefaults()
+	} else {
+		c := colly.NewCollector()
+		index := 1
+		if which_article == "ptt" {
+			c.OnHTML(".push", func(e *colly.HTMLElement) {
+				if index > output_max {
+					return
+				}
+				id := e.ChildText(".push-userid")
+				content := e.ChildText(".push-content")
+				time := e.ChildText(".push-ipdatetime")
+				fmt.Printf("%d. 名字: %s, 留言%s, 時間: %s\n", index, id, content, time)
+				index++
+			})
+			c.Visit("https://www.ptt.cc/bbs/Stock/M.1610102078.A.16E.html")
+		} else {
+			c.OnHTML("div", func(e *colly.HTMLElement) {
+				if e.Attr("id") == "tab1" {
+					e.ForEach("td.teacherInfo", func(_ int, f *colly.HTMLElement) {
+						if index > output_max {
+							return
+						}
+						name := f.ChildText(".content_title2")
+						temp := f.ChildAttrs("a", "href")
+						url := "NULL"
+						if len(temp) >= 2 && temp[1] != "" {
+							url = temp[1]
+						}
+						fmt.Printf("%d. 姓名: %s, 網站: %s\n", index, name, url)
+						index++
+					})
+				}
+			})
+			c.Visit("https://www.csie.ncku.edu.tw/ncku_csie/depmember/teacher")
+		}
+		c.Wait()
+	}
+}
